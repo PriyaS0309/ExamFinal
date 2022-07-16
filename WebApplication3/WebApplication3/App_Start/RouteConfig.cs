0029d2704b38f573a391e57203e5060bbee1100a
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-07-17 00:37:34 +0530

    Committed Final Changes

--- WebApplication3/WebApplication3/App_Start/RouteConfig.cs
+++ WebApplication3/WebApplication3/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Category", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
